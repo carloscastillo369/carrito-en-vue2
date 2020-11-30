7e4c13aa5ccf8e7a1c243f663b85451582dcfcf4
carlos castillo <ing.elec.castillo@gmail.com>
2020-11-30 11:01:05 -0500

    carrito en vue2

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    productos: [],
+    carrito: {}
   },
+
   mutations: {
+    setProductos(state, payload) {
+      state.productos = payload
+    },
+    setCarrito(state, payload) {
+      state.carrito[payload.id] = { ...payload }
+      console.log(state.carrito)
+    }
   },
   actions: {
-  },
-  modules: {
+    async fetchData({commit}) {
+      try {
+        const res = await fetch('api.json')
+        const productos = await res.json()
+        commit('setProductos', productos)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    agregarCarrito({ commit, state }, producto) {
+      state.carrito.hasOwnProperty(producto.id)
+        ? producto.cantidad = state.carrito[producto.id].cantidad + 1
+        : producto.cantidad = 1
+      commit('setCarrito', producto)
+    }
   }
 })
